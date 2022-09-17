922276bb449f483ff5e605776b3485d3b09d62d7
Dario_herrera <d.herrerag@uniandes.edu.co>
2022-09-16 19:00:27 -0500

    analyze max values v0.9

--- control/monitor.py
+++ control/monitor.py
@@ -60,15 +60,15 @@ def analyze_data():
     print(alerts, "alertas enviadas ")
 
 def analyze_data_max_temp():
-    # Consulta todos los datos de la última hora, los agrupa por estación y variable
+    # Consulta todos las temperaturas del ultimo minuto, agrupa los valores por estación y variable
     # Compara el promedio con los valores límite que están en la base de datos para esa variable.
     # Si el promedio se excede de los límites, se envia un mensaje de alerta.
 
     print("Calculando alertas...")
 
     data = Data.objects.filter(
-        base_time__gte=datetime.now() - timedelta(hours=1))
-    aggregation = data.annotate(check_value=Avg('max_value')) \
+        base_time__gte=datetime.now() - timedelta(minutes=1))
+    aggregation = data.annotate(check_value=Avg('avg_value')) \
         .select_related('station', 'measurement') \
         .select_related('station__user', 'station__location') \
         .select_related('station__location__city', 'station__location__state',
@@ -93,7 +93,7 @@ def analyze_data_max_temp():
         city = item['station__location__city__name']
         user = item['station__user__username']
 
-        if item["check_value"] > max_value or item["check_value"] < min_value:
+        if (item["check_value"] > max_value or item["check_value"] < min_value)and variable=="temperatura":
             alert = True
 
         if alert:
@@ -102,9 +102,10 @@ def analyze_data_max_temp():
             print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
             client.publish(topic, message)
             alerts += 1
+       
 
     print(len(aggregation), "dispositivos revisados")
-    print(alerts, "alertas enviadas - max values ")
+    print(alerts, "alertas enviadas - max temp ")
 
 def on_connect(client, userdata, flags, rc):
     '''
