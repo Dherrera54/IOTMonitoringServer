1a73a7b4204a6216f5fbe41fc28a178f6f60d5ed
Dario_herrera <d.herrerag@uniandes.edu.co>
2022-09-16 22:19:58 -0500

    analyze max values v1.1

--- control/monitor.py
+++ control/monitor.py
@@ -101,7 +101,7 @@ def analyze_data_max():
                 alertHum=True
 
         if alertTemp and alertHum:
-            message = "ALERT_MAX {}".format(variable, max_value)
+            message = "ALERT_MAX!"
             topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
             print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
             client.publish(topic, message)
