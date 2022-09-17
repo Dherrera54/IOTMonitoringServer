0bdf3b12640a2aa777eb6df82b50c63e541262ba
Dario_herrera <d.herrerag@uniandes.edu.co>
2022-09-17 10:31:29 -0500

    finaly2.0

--- control/monitor.py
+++ control/monitor.py
@@ -66,7 +66,7 @@ def analyze_data_max():
 
     data = Data.objects.filter(
         base_time__gte=datetime.now() - timedelta(hours=1))
-    aggregation = data.annotate(check_value=Max('max_value')) \
+    aggregation = data.annotate(check_value=Max('avg_value')) \
         .select_related('station', 'measurement') \
         .select_related('station__user', 'station__location') \
         .select_related('station__location__city', 'station__location__state',
